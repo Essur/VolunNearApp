08718b96257c42f4f4ec41e689afe730b85a02d7
Essur <asus15oleg@gmail.com>
2024-02-19 00:09:39 +0200

    Creating project and repo. First commit

---
+++ src/main/java/com/volunnear/VolunNearApplication.java
@@ -0,0 +1,13 @@
+package com.volunnear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunNearApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunNearApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/volunnear/VolunNearApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volunnear;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunNearApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
