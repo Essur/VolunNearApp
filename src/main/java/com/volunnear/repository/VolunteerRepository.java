331053897f7680a123cd0a36f9208c3d6d2dea7e
Essur <asus15oleg@gmail.com>
2024-02-24 17:47:17 +0200

    Deleted useless classes

--- src/main/java/com/volunnear/repository/VolunteerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.volunnear.repository;
-
-import com.volunnear.entitiy.TVolunteer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VolunteerRepository extends JpaRepository<TVolunteer, Long> {
-}
