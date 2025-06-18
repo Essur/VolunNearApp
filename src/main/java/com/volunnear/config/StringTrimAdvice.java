ad7b8a4a82ed7ca595288791af37d997bb582619
Essur <asus15oleg@gmail.com>
2025-06-18 19:35:46 +0300

    Added StringTrimAdvice which trim all strings when it comes to controller

---
+++ src/main/java/com/volunnear/config/StringTrimAdvice.java
@@ -0,0 +1,14 @@
+package com.volunnear.config;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class StringTrimAdvice {
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+}
